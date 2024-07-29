16e90a14f798500904032d8a029c58a6ad8cd95e
youssef-elmelegy <youssefelmelegy999@gmail.com>
2024-07-29 06:59:55 -0700

    api_data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,33 +2,26 @@
 """
 api task
 """
-import  urllib, requests
+import requests
 import sys
 
 if __name__ == "__main__":
-        url = "https://jsonplaceholder.typicode.com/"
-        id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
 
-        response_user = requests.get(f"{url}/users/{id}")
-        response_todos = requests.get(f"{url}/todos?userId={id}")
+    response_user = requests.get(f"{url}/users/{user_id}")
+    response_todos = requests.get(f"{url}/todos?userId={user_id}")
 
-        response_user = response_user.json()
-        response_todos = response_todos.json()
+    response_user = response_user.json()
+    response_todos = response_todos.json()
 
-        user_name = response_user.get("name")
+    user_name = response_user.get("name")
+    tasks_name = [task.get("title") for task in response_todos if task.get("completed")]
 
-        tasks_name = []
+    c_todos = len(response_todos)
+    c_done = len(tasks_name)
 
-
-        for task in response_todos:
-                if task.get("completed"):
-                        tasks_name.append(task.get("title"))
-
-        c_todos = len(response_todos)
-        c_done = len(tasks_name)
-
-        print(f"Employee {user_name} is done with tasks({c_done}/{c_todos})")
-        for name in tasks_name:
-                print(f"\t{name}")
-
+    print(f"Employee {user_name} is done with tasks({c_done}/{c_todos}):")
+    for name in tasks_name:
+        print(f"\t{name}")
 
