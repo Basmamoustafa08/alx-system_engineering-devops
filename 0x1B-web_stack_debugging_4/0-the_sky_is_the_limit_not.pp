8790116eb71ca24b32b810aef9951af4825ddbd1
Basmamoustafa08 <basmamoustafa08@gmail.com>
2024-08-22 23:24:40 +0300

    Update 0-the_sky_is_the_limit_not.pp1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,12 @@
-# main commint.
+# fix request limit at nginx
 
-# commmint aasjdlf
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
+}
 
 # Restart Nginx
-exec { 'nginx-restart':
+-> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
